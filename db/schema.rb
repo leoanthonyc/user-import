975cddb212a488fda6cba56fbc453da2baa507b5
Leo Catane <leoanthony.catane@protonmail.com>
2021-09-05 20:30:39 +0800

    Use postgres, sqlite is not supported by Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_09_02_005434) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "password", null: false
